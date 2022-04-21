80686fd55243ca7f215656fcf175de68ef6df9ed
TeoVH <villadamateo64@gmail.com>
2022-04-20 21:00:10 -0500

    Header file

---
+++ shell.h
@@ -0,0 +1,40 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* environment */
+
+extern char **environ;
+
+/* Shell functions */
+
+char **tokenizer(char *buffer, const char *delimiter);
+char *mini_paths(char *command);
+int new_process(char *mini_path, char **tokens);
+void char_processing(char **tokens, char *path);
+char *env_copy(void);
+
+/* Helpers */
+
+int _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+
+/* built- ins */
+
+void shell_exit(void);
+void shell_env(void);
+
+/* Free double pointer */
+
+void free_dp(char **double_pointer);
+
+#endif /* SHELL_H */
